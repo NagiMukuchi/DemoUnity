10cf1fdf4a0319969cb9a671ae397fb476563939
NagiMukuchi <NagiMukuchi@gmail.com>
2025-03-24 19:48:41 +0700

    add demo 2d trail

--- 2D Trail/Assets/Script/PlayerShooting.cs
+++ 2D Trail/Assets/Script/PlayerShooting.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using JetBrains.Annotations;
 using UnityEngine;
 
-public class ShipShooting : MonoBehaviour
+public class PlayerShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
 
@@ -12,13 +11,13 @@ public class ShipShooting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)&& Time.time - lastBulletTime > shootInterval)
+        if (Input.GetMouseButtonDown(0) && Time.time - lastBulletTime > shootInterval)
         {
             Instantiate(bulletPrefab, transform.position, transform.rotation);
             lastBulletTime = Time.time;
