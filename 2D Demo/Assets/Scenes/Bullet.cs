750544ff2d0ae7b0d67ab8d70761bc3011581a2e
NagiMukuchi <NagiMukuchi@gmail.com>
2025-03-28 21:06:57 +0700

    add demo 2d

--- 2D Demo/Assets/Scenes/Bullet.cs
+++ 2D Demo/Assets/Scenes/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float flySpeed = 20;
+    public int damage;
 
     // Update is called once per frame
     void Update()
@@ -13,6 +14,17 @@ public class Bullet : MonoBehaviour
         newPosition.y += Time.deltaTime * flySpeed;
         transform.position = newPosition;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var enemy = collision.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+
 }
 
 
