c784311a290b26286ea28525d4f0b017d72cf832
NagiMukuchi <NagiMukuchi@gmail.com>
2025-03-31 20:54:51 +0700

    add demo 2d

---
+++ 2D new/Assets/TextSample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSample : MonoBehaviour
+{
+    [SerializeField] Text myText;
+    float lastTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myText.text = "Hello World!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastTime >= 1)
+        {
+            myText.text = $"{1/Time.deltaTime:####.##}FPS!";
+            lastTime = Time.time;
+        }
+    }
+
+
+}
